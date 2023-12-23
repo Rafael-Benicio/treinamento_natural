13144833bdc96d14c60999ae43045991c20b6f1d
Rafael-Benicio <jerobenicio@gmail.com>
2023-12-23 11:25:25 -0300

    fix(testforfundamental/urls.py): adicinar um view para ser visualizada quando o usuario está no '/'

--- Python/Django/Prova_Fundamental/testForFundamental/urls.py
+++ Python/Django/Prova_Fundamental/testForFundamental/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path,include
+import school_test.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("school_test/", include("school_test.urls")),
+    path("", school_test.views.index),
 ]
