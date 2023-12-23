1b3db82c348d757de5b0aa9922db08e78eb084ab
Rafael-Benicio <jerobenicio@gmail.com>
2023-12-23 14:11:47 -0300

    feat(view,url e template da prova): cria uma url, uma view, um contexto e o template test.html

--- Python/Django/Prova_Fundamental/school_test/urls.py
+++ Python/Django/Prova_Fundamental/school_test/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("", school_test.views.index, name="index"),
     path("validate_user/", school_test.views.user_login_validate, name="validate"),
     path("<int:id_student>/home/", school_test.views.home, name="home"),
+    path("<int:id_student>/home/prova/<int:id_test>", school_test.views.student_test, name="test"),
     # path("<int:id_student>/test/", school_test.views.home, name="home"),
 ]
--- Python/Django/Prova_Fundamental/school_test/views.py
+++ Python/Django/Prova_Fundamental/school_test/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
-from .models import Student, TestToDo, ReadyTest
+from .models import Student, TestToDo, ReadyTest,Question, TestQuestions
 
 def index(request):
      return render(request, "school_test/index.html")
@@ -27,3 +27,17 @@ def home(request, id_student):
      # tests=[ print(student_test.id_test.id) for student_test in student_tests]
      context = {"student_tests": tests}
      return render(request, "school_test/home.html",context)
+
+def student_test(request, id_student,id_test):
+     test_inf=ReadyTest.objects.get(id=int(id_test))
+     test_questions=TestQuestions.objects.filter(id_test=id_test)
+     questions_from_test=[Question.objects.get(id=question_id.id) for question_id in test_questions]
+     # [print(question_id) for question_id in test_questions]
+     test={'name':test_inf.test_name}
+     test['id_student']=id_student
+     test['subject']=test_inf.subject
+     test['questions']=questions_from_test
+     context = {"test": test}
+     # {name=test.name}
+     return render(request, "school_test/test.html",context)
+     
