a4906fbd998126629ba5c469b9a3774ca980778e
Rafael-Benicio <jerobenicio@gmail.com>
2023-12-26 17:36:55 -0300

    refactor(view.py): remove responsabilidades das views e as transfere para funções especificas

--- Python/Django/Prova_Fundamental/school_test/views.py
+++ Python/Django/Prova_Fundamental/school_test/views.py
@@ -13,7 +13,6 @@ def user_login_validate(request):
      id_student=request.POST["id_student"]
      try:
           student_entity = Student.objects.get(id=int(id_student))
-          # if student_entity.password != request.POST['password']:
           if not (ph.verify(student_entity.password, request.POST['password'])):
                raise "Wrong Password"
      except:
@@ -21,50 +20,17 @@ def user_login_validate(request):
      else:
           return HttpResponseRedirect(reverse("school_test:home",args=(id_student,)))
 
-
-
-def home(request, id_student)->HttpResponse:
-     associated_tests=[]
-     id_from_student_and_readytests=TestToDo.objects.filter(id_student=id_student)
-     for testtodo_object in id_from_student_and_readytests:
-          test_entity=(ReadyTest.objects.get(id=testtodo_object.id_test.id))
-
-          test_object={"test_name":test_entity.test_name}
-          test_object["subject"]=test_entity.subject
-          test_object["id"]=test_entity.id
-          test_object["grade"]=testtodo_object.grade
-          test_object["was_done"]=testtodo_object.was_done
-
-          associated_tests.append(test_object)
-
-     context = {"student_tests": associated_tests}
+def home(request, id_student:int)->HttpResponse:
+     context = {"student_tests": build_list_of_tests(id_student)}
      return render(request, "school_test/home.html",context)
 
 def student_test(request, id_student,id_test):
-     questions=[]
-     student_test=ReadyTest.objects.get(id=int(id_test))
-
-     id_from_question_and_readytest=TestQuestions.objects.filter(id_test=id_test)
-     for testquestion_object in id_from_question_and_readytest:
-          questions.append(Question.objects.get(id=testquestion_object.id_question.id))
-
-     test={'name':student_test.test_name, }
-     test['id_student']=id_student
-     test['id_test']=id_test
-     test['subject']=student_test.subject
-     test['questions']=questions
-
-     context = {"test": test}
-
-     # {name=test.name}
+     context = {"test": build_student_test(id_student,id_test)}
      return render(request, "school_test/test.html",context)
 
 def result_calculate(request):
      grade=calculate_test_grade(remove_identification_data(request.POST))
-     student_test=TestToDo.objects.get(id_test=int(request.POST["id_test"]), id_student=int(request.POST["id_student"]))
-     student_test.grade=grade
-     student_test.was_done=True
-     student_test.save()
+     regist_student_test_grade(request.POST,grade)
      return HttpResponseRedirect(reverse("school_test:home",args=(request.POST["id_student"],)))
 
 def calculate_test_grade(id_question_and_choice:dict)-> float:
@@ -78,6 +44,42 @@ def calculate_test_grade(id_question_and_choice:dict)-> float:
      
      return right_answers*weight
 
+def regist_student_test_grade(test_form:dict,grade:float)->None:
+     student_test=TestToDo.objects.get(id_test=int(test_form["id_test"]), id_student=int(test_form["id_student"]))
+     student_test.grade=grade
+     student_test.was_done=True
+     student_test.save()
+
 def remove_identification_data(test_form:dict)->dict:
      return {field_name:test_form[field_name] for field_name in test_form if field_name!='id_student' and field_name!='csrfmiddlewaretoken' and field_name!='id_test'}
     
+def build_list_of_tests(id_student:int)-> list[dict]:
+     associated_tests=[]
+     id_from_student_and_readytests=TestToDo.objects.filter(id_student=id_student)
+     for testtodo_object in id_from_student_and_readytests:
+          test_entity=(ReadyTest.objects.get(id=testtodo_object.id_test.id))
+
+          test_object={"test_name":test_entity.test_name}
+          test_object["subject"]=test_entity.subject
+          test_object["id"]=test_entity.id
+          test_object["grade"]=testtodo_object.grade
+          test_object["was_done"]=testtodo_object.was_done
+
+          associated_tests.append(test_object)
+     return associated_tests
+
+def build_student_test(id_student:int,id_test:int)->dict:
+     questions=[]
+     student_test=ReadyTest.objects.get(id=int(id_test))
+     test_was_done=TestToDo.objects.get(id_test=int(id_test), id_student=int(id_student)).was_done
+     id_from_question_and_readytest=TestQuestions.objects.filter(id_test=id_test)
+     for testquestion_object in id_from_question_and_readytest:
+          questions.append(Question.objects.get(id=testquestion_object.id_question.id))
+
+     test={'name':student_test.test_name, }
+     test['id_student']=id_student
+     test['id_test']=id_test
+     test['subject']=student_test.subject
+     test['questions']=questions
+     test['was_done']=test_was_done
+     return test
