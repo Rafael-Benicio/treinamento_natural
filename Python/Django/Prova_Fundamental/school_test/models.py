04aca64ba9686520c05c3d3f277d04a6a017aeaf
Rafael-Benicio <jerobenicio@gmail.com>
2024-01-01 19:52:39 -0300

    refactor : Adiciona a visualização do Id no modelo Studant

--- Python/Django/Prova_Fundamental/school_test/models.py
+++ Python/Django/Prova_Fundamental/school_test/models.py
@@ -27,7 +27,7 @@ class Student(models.Model):
      last_name=models.CharField(max_length=20,null=False)
      password=models.CharField(max_length=100,null=False)
      def __str__(self):
-        return f"Aluno : {self.first_name} {self.last_name}"
+        return f"ID : {self.id} | Aluno : {self.first_name} {self.last_name}"
 
 # Prova(Id_Prova, Nome_Prova ,Materia)
 class ReadyTest(models.Model):
